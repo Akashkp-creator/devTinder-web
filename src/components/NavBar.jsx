aaa87243825cbcf0cd8a0f883b68244393b8f8c0
Akashkp-creator <akp979760@gmail.com>
2025-08-29 12:40:28 +0530

    Redirected to login page if not authorized

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,15 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
-  console.log(user);
+  // console.log(user);
   return (
     <div className="navbar bg-secondary-content shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">😊DevTinder</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          😊DevTinder
+        </Link>
       </div>
 
       {user && (
@@ -27,10 +30,10 @@ const NavBar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>

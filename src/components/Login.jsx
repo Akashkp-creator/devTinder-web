2472e0163dde7a0aa6ba523aa59c1745faa2dbc6
Akashkp-creator <akp979760@gmail.com>
2025-08-29 10:06:19 +0530

    Setup Redux Store, userSlice, Get LoggedIn user from BackEnd

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,32 @@
 import axios from "axios";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [emailId, setEmail] = useState("akash@gmail.com");
   const [password, setPassword] = useState("Akash@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const handleLogin = async () => {
-    console.log(emailId, password);
+    // console.log(emailId, password);
+
     try {
       const res = await axios.post(
-        "http://localhost:3000/login",
+        BASE_URL + "/login",
         {
           emailId,
           password,
         },
         { withCredentials: true }
       );
-      console.log(res);
-      console.log(emailId, password);
+      // console.log(res.data);
+      dispatch(addUser(res.data));
+      return navigate("/");
+      // console.log(emailId, password);
     } catch (error) {
       console.log(error);
     }
